adba2cc35ce1168c097277a08a840658bb77ab1f
Sourav Pandya <souravpandya00@Souravs-MacBook-Pro.local>
2021-01-21 23:02:51 -0500

    First commit

---
+++ DecisionMaker/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // IBOutlet allows me to connect to the UI
+    @IBOutlet weak var coinImageView1: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        coinImageView1.image = #imageLiteral(resourceName: "Coin")
+        diceImageView1.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceOne")
+    }
+
+    @IBAction func flipButtonPressed(_ sender: UIButton) {
+        coinImageView1.image = [#imageLiteral(resourceName: "Heads"), #imageLiteral(resourceName: "Tails")][Int.random(in: 0...1)]
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+    }
+    
+    
+}
+
